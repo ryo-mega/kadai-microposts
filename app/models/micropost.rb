70ce73a4506467344da5f8f90a13b1c1b81482f5
ryosuke.mega <ryosuke.mega@gmail.com>
2020-07-08 13:14:26 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :favorites
+  has_many :favoritings, through: :reverses_of_favorites, source: :users_id
+
 
   validates :content, presence: true, length: { maximum: 255 }
 end
